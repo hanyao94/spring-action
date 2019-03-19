2c60ff2f9bbe46eed3afba9a80bf84d8f32074c4
hanyao94 <hanyao.huang@foxmail.com>
2019-03-19 20:56:14 +0800

    通过javaconfig、xml混合装配bean

--- Chapter_02/src/main/java/com/seven/spring/action/mixconfig/SoundSystemConfig.java
+++ Chapter_02/src/main/java/com/seven/spring/action/mixconfig/SoundSystemConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @Import(CDPlayerConfig.class) //将两个配置类结合在一起
-@ImportResource("classpath:META-INF/mixconfig/cd-config.xml") //讲配置文件和配置类结合在一起
+@ImportResource("classpath:META-INF/mixconfig/cd-config.xml") //将配置文件和配置类结合在一起
 public class SoundSystemConfig {
 
 }
