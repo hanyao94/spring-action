2c960d808f2cc4838ac5c65b22c431f4c081e05e
hanyao94 <hanyao.huang@foxmail.com>
2019-03-20 15:45:09 +0800

    高级装配处理自动装配的歧义性 @qualifier 自定义

--- Chapter_03/src/test/java/ambiguity/AmbiguityTest.java
+++ Chapter_03/src/test/java/ambiguity/AmbiguityTest.java
@@ -11,15 +11,11 @@ package ambiguity;
 
 import com.seven.spring.action.ambiguity.AmbugeConfig;
 import com.seven.spring.action.ambiguity.Eat;
-import com.seven.spring.action.conditional.MagicConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * @author seven
