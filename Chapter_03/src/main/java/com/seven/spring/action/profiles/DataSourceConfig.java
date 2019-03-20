1ca3ed95ba43bfba6642e660d1a3c5826d3003a6
hanyao94 <hanyao.huang@foxmail.com>
2019-03-20 13:32:22 +0800

    高级装配profiles

---
+++ Chapter_03/src/main/java/com/seven/spring/action/profiles/DataSourceConfig.java
@@ -0,0 +1,47 @@
+/**
+ * 版权所有(C)，上海海鼎信息工程股份有限公司，2016，所有权利保留。
+ * <p>
+ * 项目名：	spring-action
+ * 文件名：	DataSourceConfig.java
+ * 模块说明：
+ * 修改历史：
+ * 2019/3/20 - seven - 创建。
+ */
+package com.seven.spring.action.profiles;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
+
+/**
+ * @author seven
+ */
+@Configuration
+public class DataSourceConfig {
+
+  @Bean(destroyMethod = "shutdown")
+  @Profile("dev")
+  public DataSource embeddedDataSource() {
+    return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript("classpath:META-INF/profiles/shcema.sql")
+            .addScript("classpath:META-INF/profiles/test-data.sql")
+            .build();
+  }
+
+  @Bean
+  @Profile("prod")
+  public DataSource jndiDataSource() {
+    JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+    jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+    jndiObjectFactoryBean.setResourceRef(true);
+    jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+    return (DataSource) jndiObjectFactoryBean.getObject();
+  }
+
+}
---
+++ Chapter_03/src/test/java/profiles/DataSourceConfigTest.java
@@ -0,0 +1,109 @@
+/**
+ * 版权所有(C)，上海海鼎信息工程股份有限公司，2016，所有权利保留。
+ * <p>
+ * 项目名：	spring-action
+ * 文件名：	DataSourceConfigTest.java
+ * 模块说明：
+ * 修改历史：
+ * 2019/3/20 - seven - 创建。
+ */
+package profiles;
+
+import com.seven.spring.action.profiles.DataSourceConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author seven
+ */
+public class DataSourceConfigTest {
+
+  @RunWith(SpringJUnit4ClassRunner.class)
+  @ContextConfiguration(classes = DataSourceConfig.class)
+  @ActiveProfiles("dev")
+  public static class DevDataSourceTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void shouldBeEmbeddedDatasource() {
+      assertNotNull(dataSource);
+      JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+      List<String> results = jdbc.query("select id, name from Things", new RowMapper<String>() {
+
+        public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+          return rs.getLong("id") + ":" + rs.getString("name");
+        }
+      });
+
+      assertEquals(1, results.size());
+      assertEquals("1:A", results.get(0));
+    }
+  }
+
+  @RunWith(SpringJUnit4ClassRunner.class)
+  @ContextConfiguration(classes = DataSourceConfig.class)
+  @ActiveProfiles("prod")
+  public static class ProductionDataSourceTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void shouldBeEmbeddedDatasource() {
+      // should be null, because there isn't a datasource configured in JNDI
+      assertNull(dataSource);
+    }
+  }
+
+  @RunWith(SpringJUnit4ClassRunner.class)
+  @ContextConfiguration("classpath:META-INF/profiles/datasource-config.xml")
+  @ActiveProfiles("dev")
+  public static class DevDataSourceTest_XMLConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void shouldBeEmbeddedDatasource() {
+      assertNotNull(dataSource);
+      JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+      List<String> results = jdbc.query("select id, name from Things", new RowMapper<String>() {
+
+        public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+          return rs.getLong("id") + ":" + rs.getString("name");
+        }
+      });
+
+      assertEquals(1, results.size());
+      assertEquals("1:A", results.get(0));
+    }
+  }
+
+  @RunWith(SpringJUnit4ClassRunner.class)
+  @ContextConfiguration("classpath:META-INF/profiles/datasource-config.xml")
+  @ActiveProfiles("prod")
+  public static class ProductionDataSourceTest_XMLConfig {
+    @Autowired(required = false)
+    private DataSource dataSource;
+
+    @Test
+    public void shouldBeEmbeddedDatasource() {
+      // should be null, because there isn't a datasource configured in JNDI
+      assertNull(dataSource);
+    }
+  }
+}
