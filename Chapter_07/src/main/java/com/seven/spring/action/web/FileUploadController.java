d7f0ea3762f14e0540bec5564f0ad223bbbaa638
hanyao94 <hanyao.huang@foxmail.com>
2019-04-14 22:53:14 +0800

    第7章，通过上传文件，通过StandardServletMultipartResolver 和 StandardServletMultipartResolver 实现

---
+++ Chapter_07/src/main/java/com/seven/spring/action/web/FileUploadController.java
@@ -0,0 +1,41 @@
+/**
+ * 版权所有(C)，上海海鼎信息工程股份有限公司，2016，所有权利保留。
+ * <p>
+ * 项目名：	spring-action
+ * 文件名：	FileUploadController.java
+ * 模块说明：
+ * 修改历史：
+ * 2019/4/14 - seven - 创建。
+ */
+package com.seven.spring.action.web;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author seven
+ */
+@Controller
+@RequestMapping("/fileupload")
+public class FileUploadController {
+
+  @RequestMapping(method = RequestMethod.POST)
+  public String processUpload(@RequestPart("file") MultipartFile file) throws IOException {
+    System.out.println(file.getSize());
+    //在这里就可以对file进行处理了，可以根据自己的需求把它存到数据库或者服务器的某个文件夹
+    //获取图片名称
+    String fileName = file.getOriginalFilename();
+    //获取图片扩展名
+    String ext = FilenameUtils.getExtension(file.getOriginalFilename());
+    String direct = "D:/spring/spring-action/Chapter_07/src/main/webapp/resources";
+    file.transferTo(new File(direct + "/" + fileName + "." + ext));
+    return "redirect:/";
+  }
+}
--- Chapter_07/src/main/webapp/index.jsp
+++ Chapter_07/src/main/webapp/index.jsp
@@ -1,5 +1,21 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme() + "://"
+            + request.getServerName() + ":" + request.getServerPort()
+            + path + "/";
+%>
+
 <html>
 <body>
 <h2>Hello World!</h2>
+<br/>
+
+<form action="<%=path %>/fileupload" enctype="multipart/form-data" method="POST">
+
+    selectimage: <input type="file" name="file"/><br>
+    <input type="submit" value="upload"/>
+</form>
+
 </body>
 </html>
